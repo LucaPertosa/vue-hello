e0939501985a4d3a74b2b64a523a61200bd267cd
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-28 17:03:51 +0200

    Add: bottone cambia colore al titolo + img

--- js/script.js
+++ js/script.js
@@ -5,6 +5,19 @@ createApp({
         return {
             headingText: "Ciao sono un messaggio scritto con Vue",
             headingClass: "red",
+            imgSrc: "img/01.jpeg",
+            imgAlt: "Thayland GEZI"
         }
     },
+    methods: {
+        changeColor() {
+            if (this.headingClass === "red") {
+                this.headingClass = "blue"
+            } else if (this.headingClass === "blue") {
+                this.headingClass = "green"
+            } else {
+                this.headingClass = "red"
+            }
+        }
+    }
 }).mount('#app');
